924630fdfc8ccb39b1357f16836e18a53dc57e8a
Laura <lafa0001@stud.kea.dk>
2024-12-06 12:26:37 +0100

    farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      wine: "#6E1E3A",
+      rose: "#CDA7B2",
+      darkgrey: "#2F3642",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
